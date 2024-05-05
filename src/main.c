e7f2f029c5269f6b596120ebd244e84b6d856ca6
sherllyneo <jacobp.willo@gmail.com>
2024-05-05 15:45:26 +0100

    added insert row restriction

--- src/main.c
+++ src/main.c
@@ -28,7 +28,7 @@ int main()
 
     /* insert a row - must be malloc'd so the row can be freed later. strdup will malloc for us. 
      * Be careful, here we insert 5 into a csv of width 4, the insert function will get rid of element 5 "five" and turn the excess elements to null (and null terminate), 
-     * effectively silently trunacting */
+     * effectively silently trunacting and MUST be a null terminated array */
 
      char** row = (char**)malloc(sizeof(char*) * 4);
     if (!row) {
